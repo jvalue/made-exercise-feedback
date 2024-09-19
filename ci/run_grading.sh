42159efe1952e0b12868797eb0ab20016057e044
Georg Schwarz <schwarz@group.riehle.org>
2024-09-19 15:48:42 +0200

    Change CI script to execute grading for all exercises

--- ci/run_grading.sh
+++ ci/run_grading.sh
@@ -1,4 +1,8 @@
 cd grading
 pip install -r requirements.txt
 
-python grading.py "../main" 2>&1 | stdbuf -oL tee feedback-log.txt
+python grading.py "../main" 1 2>&1 | stdbuf -oL tee feedback-ex1.txt
+python grading.py "../main" 2 2>&1 | stdbuf -oL tee feedback-ex2.txt
+python grading.py "../main" 3 2>&1 | stdbuf -oL tee feedback-ex3.txt
+python grading.py "../main" 4 2>&1 | stdbuf -oL tee feedback-ex4.txt
+python grading.py "../main" 5 2>&1 | stdbuf -oL tee feedback-ex5.txt
