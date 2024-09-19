16b60520a88a385a557ddf0d312d182c2c21cb76
Georg Schwarz <schwarz@group.riehle.org>
2024-09-19 15:59:51 +0200

    Fix run_grading.sh script

--- ci/run_grading.sh
+++ ci/run_grading.sh
@@ -1,8 +1,8 @@
 cd grading
 pip install -r requirements.txt
 
-python grading.py "../main" 1 2>&1 | stdbuf -oL tee feedback-ex1.txt
-python grading.py "../main" 2 2>&1 | stdbuf -oL tee feedback-ex2.txt
-python grading.py "../main" 3 2>&1 | stdbuf -oL tee feedback-ex3.txt
-python grading.py "../main" 4 2>&1 | stdbuf -oL tee feedback-ex4.txt
-python grading.py "../main" 5 2>&1 | stdbuf -oL tee feedback-ex5.txt
+python grading.py 1 "../main" 2>&1 | stdbuf -oL tee feedback-ex1.txt
+python grading.py 2 "../main" 2>&1 | stdbuf -oL tee feedback-ex2.txt
+python grading.py 3 "../main" 2>&1 | stdbuf -oL tee feedback-ex3.txt
+python grading.py 4 "../main" 2>&1 | stdbuf -oL tee feedback-ex4.txt
+python grading.py 5 "../main" 2>&1 | stdbuf -oL tee feedback-ex5.txt
